b1b0ecef147d26bce7971243a9c562e0cef783dd
MacPu <609779377@qq.com>
2015-11-10 23:50:40 +0800

    add  MPEmojiKeyboardGroupView

---
+++ MPEmojiKeyboard/MPEmojiKeyboard/MPEmojiKeyboardGroupView.h
@@ -0,0 +1,13 @@
+//
+//  MPEmojiKeyboardGroupView.h
+//  MPEmojiKeyboard
+//
+//  Created by 蒲德贵 on 15/11/10.
+//  Copyright © 2015年 MacPu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPEmojiKeyboardGroupView : UIView
+
+@end
---
+++ MPEmojiKeyboard/MPEmojiKeyboard/MPEmojiKeyboardGroupView.m
@@ -0,0 +1,21 @@
+//
+//  MPEmojiKeyboardGroupView.m
+//  MPEmojiKeyboard
+//
+//  Created by 蒲德贵 on 15/11/10.
+//  Copyright © 2015年 MacPu. All rights reserved.
+//
+
+#import "MPEmojiKeyboardGroupView.h"
+
+@implementation MPEmojiKeyboardGroupView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
